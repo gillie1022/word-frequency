d2cd840f3cdbdb43308ea430ae90a7bd9cc43a3f
gillie1022 <dave.gillie@icloud.com>
2020-05-19 18:51:21 -0400

    breaking for dinner

--- word_frequency.py
+++ word_frequency.py
@@ -5,9 +5,47 @@ STOP_WORDS = [
 ]
 
 
+def clean_text(text):
+    import string
+    text = text.lower()
+    text_to_keep = ""
+    for char in text:
+        if not char in string.punctuation:
+            text_to_keep += char
+    return text_to_keep
+
+def remove_from_list(list_of_items, items_to_remove):
+    new_list = []
+    for item in list_of_items:
+        if not item in items_to_remove:
+            new_list.append(item)
+    return(new_list)
+
+def get_word_dict(word_list):
+    word_dict = {}
+    for idx in range(len(word_list)):
+        word_dict[word_list[idx]] = word_list.count(word_list[idx])
+    return word_dict
+
+def format_output(sorted_dict):
+    for word in sorted_dict:
+        return f""
+
+
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    opened_file = file.open()
+    text = opened_file.read()
+    words = clean_text(text).split()
+    word_list = remove_from_list(words, STOP_WORDS)
+    word_dict = get_word_dict(word_list)
+    sorted_dict = sorted(word_dict.items(), key = lambda x: x[1], reverse=True)
+    print(sorted_dict)
+        
+
+
+
+
 
 
 if __name__ == "__main__":
